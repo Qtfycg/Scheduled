c4e4d391538921f6f9ed8db45b17c229de61edff
qtfycg <1264318507@qq.com>
2024-11-29 19:34:06 +0800

    Arco

--- front/src/main.js
+++ front/src/main.js
@@ -1,10 +1,17 @@
 import './assets/main.css'
 import {createApp} from 'vue'
 import App from './App.vue'
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-app.mount('#app')
+app.mount('#app');
+app.use(ArcoVue);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
+
+
+
+
