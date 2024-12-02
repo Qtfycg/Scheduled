ed0511e969954b6de444ae94cbf7765bbbfa2d0d
qtfycg <1264318507@qq.com>
2024-12-02 18:22:41 +0800

    12.3

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-
+const url = "src\components\login.jpg"
 </script>
 
 <template>
 
 <template>
   <div class="login">  //登录页面
-    <img src="../public/login.jpg" alt="logo" />
     <el-container>
+        <div class="building"></div>
       <!-- 主要部分 -->
       <el-main>
         <div class="login-imge">
@@ -21,5 +21,25 @@
 </template>
 
 <style scoped>
+#app{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0px;
+}
+body{
+  margin:0;
+  padding:0;
+  border:0
+}
+#building{
+  background:url("../src/components/login.jpg");
+  width:100%;
+  height:100%;
+  position:fixed;
+  background-size:100% 100%;
+}
 
 </style>
